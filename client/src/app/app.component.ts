e57f5294b0ec5dd75d726ae9ceedf960906b035c
Naim Jeem <naim36208@gmail.com>
2019-10-23 07:31:42 +0600

    angular Client

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,190 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpRequest, HttpEventType } from '@angular/common/http';
+import { 
+  MatSnackBar, 
+  MatSnackBarConfig ,
+  MatSnackBarVerticalPosition, 
+  MatSnackBarHorizontalPosition
+} from '@angular/material';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent implements OnInit {
+  selectedFile;
+  // fReader;
+  // name = '';
+  uploadPercent;
+
+  color = 'primary';
+  mode = 'determinate';
+  // value = 50.25890809809;
+
+  selectedFiles = [];
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  snackBarConfig: MatSnackBarConfig = {
+    duration: 5000,
+    panelClass: 'blue-snackbar',
+    verticalPosition: this.verticalPosition,
+    horizontalPosition: this.horizontalPosition
+  };
+
+  constructor(
+    private http: HttpClient,
+    public snackBar: MatSnackBar,
+  ) {}
+
+  ngOnInit() {
+   
+  }
+
+  dragOverHandler(event) {
+    console.log('files in drop zone');
+
+    event.preventDefault();
+    event.stopPropagation();
+    
+  }
+
+  dropHandler(event) {
+    event.preventDefault();
+    console.log(event.dataTransfer.items.length);
+    
+    if (event.dataTransfer.items) {
+      for (let i = 0; i < event.dataTransfer.items.length; i++) {
+        if (event.dataTransfer.items[i].kind === 'file') {
+          let file = event.dataTransfer.items[i].getAsFile();
+          let obj = {
+            fileName: file.name,
+            selectedFile: file,
+            fileId: `${file.name}-${file.lastModified}`,
+            uploadCompleted: false
+          }
+
+          this.selectedFiles.push(obj);
+          console.log(this.selectedFiles);
+          
+          console.log('...file[' + i + '].name = ' + file.name);
+        }
+      }
+
+      this.selectedFiles.forEach(file => this.getFileUploadStatus(file));
+    } else {
+      for (let i = 0; i < event.dataTransfer.files.length; i++) {
+        console.log('====================================');
+        console.log('...file[' + i + '].name = ' + event.dataTransfer.files.name);
+        console.log('====================================');
+      }
+    }
+  }
+
+  onFileSelect(event) {
+    if (event.target.files) {
+      for (let i = 0; i < event.target.files.length; i++) {
+        let file = event.target.files[i];        
+        let obj = {
+          fileName: file.name,
+          selectedFile: file,
+          fileId: `${file.name}-${file.lastModified}`,
+          uploadCompleted: false
+        }
+
+        this.selectedFiles.push(obj);
+        console.log(this.selectedFiles);
+        
+        console.log('...file[' + i + '].name = ' + file.name);
+
+      }
+
+      this.selectedFiles.forEach(file => this.getFileUploadStatus(file));
+    }
+  }
+
+  uploadFiles() {
+    console.log(this.selectedFiles.length);
+
+    if (this.selectedFiles.length <= 5) {
+      this.selectedFiles.forEach(file => {
+        if (file.uploadPercent < 100) {
+          this.resumeUpload(file);
+        }
+      });
+    } else {
+      console.log('max 5 limit');
+      this.snackBar.open('Maximum 5 Files Are Allowed', 'close', this.snackBarConfig);
+    }
+    
+  }
+
+  getFileUploadStatus(file) {
+    let headers = new HttpHeaders({
+      'size': file.selectedFile.size.toString(),
+      'x-file-id': file.fileId,
+      'name': file.fileName
+    });
+
+    this.http
+      .get('http://localhost:3000/status', { headers: headers })
+        .subscribe((res: any) => {
+          file.uploadedBytes = res.uploaded;
+          file.uploadPercent = Math.round(100 * file.uploadedBytes / file.selectedFile.size);
+
+          if (file.uploadPercent >= 100) {
+            file.uploadCompleted = true;
+          }
+        }, err => {
+          console.log(err);
+        });
+  }
+
+  resumeUpload(file) {
+    // let fileId = `${this.selectedFile.name}-${this.selectedFile.lastModified}`;
+    let headers2 = new HttpHeaders({
+      'size': file.selectedFile.size.toString(),
+      'x-file-id': file.fileId,
+      'x-start-byte': file.uploadedBytes.toString(),
+      'name': file.fileName
+    });
+
+    // console.log(JSON.stringify(res));
+    // if(res.status === 'file is present'){
+    //   alert(res.status);
+    //   return;
+    // }
+
+    const req = new HttpRequest('POST', 'http://localhost:3000/upload', file.selectedFile.slice(file.uploadedBytes, file.selectedFile.size + 1), {
+      headers: headers2,
+      reportProgress: true
+    });
+    this.http.request(req)
+      .subscribe(
+        (res: any) => {
+          if (res.type === HttpEventType.UploadProgress){
+            file.uploadPercent = Math.round(100 * (file.uploadedBytes + res.loaded)/res.total);
+            console.log(file.uploadPercent);
+            if(file.uploadPercent >= 100){
+              file.uploadCompleted = true;
+            }
+          }else{
+            console.log(JSON.stringify(res));
+            if(this.uploadPercent >= 100){
+              file.uploadCompleted = true;
+            }
+          }
+        },
+        err => {}
+      );
+  }
+
+  deleteFile(file) {
+    this.selectedFiles.splice(this.selectedFiles.indexOf(file), 1);
+    console.log('====================================');
+    console.log(file);
+    console.log('====================================');
+  }
+}
